519a30c82abbf6c1b6f00d98222fc5bf29932d6b
ScottDavis08 <Scott.Davis08@gmail.com>
2025-04-01 15:35:59 -0600

    feat: Working process to create crime records from CLI, and print them as raw JSON to teh screen.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import crimeRecordRoutes from './routes/crimeRecordRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', (req, res) => {
-    res.send('This is our great crime app.');
-})
+app.use('/', crimeRecordRoutes);
 
 const server = app.listen(PORT, () => {
     console.log(`server listening on PORT ${PORT}`);
