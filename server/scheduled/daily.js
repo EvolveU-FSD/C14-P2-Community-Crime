68122415eee7ac71b633ed0426a6f2a43e13f1f8
ScottDavis08 <Scott.Davis08@gmail.com>
2025-04-02 21:26:02 -0600

    docs: Some todo documentation about what to remove later.

--- server/scheduled/daily.js
+++ server/scheduled/daily.js
@@ -9,10 +9,11 @@ export const importCrimeData = async () => {
         const response = await fetch("https://data.calgary.ca/resource/78gh-n26t.json");
         const crimeData = await response.json();
 
+        //TODO: remove when writing more than 20 records.
         let i = 0;
         // Process and save each crime.
         for (const crimeRecord of crimeData) {
-            // console.log(crimeRecord);
+            // Send a request to Crimes to create a new record for each coming from the API.
             const newCrimeRecord = await findOneAndUpdate(
                crimeRecord.community,
                crimeRecord.category,
@@ -20,7 +21,9 @@ export const importCrimeData = async () => {
                crimeRecord.year,
                crimeRecord.month 
             )
+            // TODO: remove- Temporarily writing the new record created.
             console.log(`Created ${newCrimeRecord}`);
+            //TODO: remove- Breaking after writing about 20 records.
             i++;
             if (i > 20) break;
         }
