2d4d8dd50e3004b1abf583747d858ccb4159d3ee
ScottDavis08 <Scott.Davis08@gmail.com>
2025-04-02 21:23:09 -0600

    feat: Tied the API to pull the first 20 records and save them to db.

--- server/models/crimes.js
+++ server/models/crimes.js
@@ -39,6 +39,42 @@ export async function createCrimeRecord(community, category, crimeCount, year, m
     return newCrime;
 }
 
+// It's great to create the crime record above, but we also want the option to update a record if it already exists
+// or not insert a new one if it overlaps.
+// This will attempt to find existing records and update them if necessary.
+export async function findOneAndUpdate(community, category, crimeCount, year, month) {
+    try {
+        // Similar to .create() or .find(), mongoose also has a built in function to find something based on a filter, 
+        // and update or create it.
+        // We start by setting the fields that can't change and are unique.
+        const filter = {
+            community: community,
+            category: category,
+            year: year,
+            month: month
+        }
+
+        // We then set the only field that might change in value.
+        const update = {
+            ...filter,
+            crimeCount: crimeCount
+        }
+
+        // The options flag ensures if the record exists, update it but if it doesn't exist, create it (upsert)
+        // and that the return object is the updated object (by default the original record is returned).
+        const options = {
+            upsert: true,
+            new: true
+        }
+
+        const newCrime = await CrimeRecord.findOneAndUpdate(filter, update, options);
+        return newCrime;
+    } catch (error) {
+        console.error(`Error in findOneAndUpdate: ${error}`);
+        throw error;
+    }
+}
+
 // Use the CrimeRecord model to find all records in the collection/table. 
 // In this case the model is built on the "crimes" collection. 
 export async function findAllCrimeRecords() {
