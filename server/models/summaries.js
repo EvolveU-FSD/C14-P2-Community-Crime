49b43a81d3c9b8b309c98ddca6813914219e1e69
ScottDavis08 <Scott.Davis08@gmail.com>
2025-04-12 11:00:07 -0600

    docs: Documentation for the summaries functions.

--- server/models/summaries.js
+++ server/models/summaries.js
@@ -3,14 +3,19 @@ import { CrimeRecord } from "./crimes.js";
 
 const mongoose = await connectDb();
 
+// We want to by default display all our cimes as a total on the main map.
+// This function will group all of the results first by community, summarize them,
+// then will summarize them by crime.
 export async function getCrimesByCommunity() {
     try {
         const crimeSummary = await CrimeRecord.aggregate([
             {
+                // Within Mongoose create a group variable. The key is the community field and will then crime by that value.
                 $group: {
                     _id: "$community",
                     totalCrimes: { $sum: "$crimeCount"},
                     crimesByCategory: {
+                        // In addition to the original grouping, a second grouping exists to also split by crime.
                         $push: {
                             category: "$category",
                             count: "$crimeCount"
@@ -19,6 +24,7 @@ export async function getCrimesByCommunity() {
                 }
             },
             {
+                // sorting by totalCrimes -1 is descending order.
                 $sort: { totalCrimes: -1 }
             }
         ]);
@@ -29,6 +35,8 @@ export async function getCrimesByCommunity() {
     }
 }
 
+// Similar to the function above, this one instead gets information just for one community
+// that is passed in and gets the summary. This will be good for individual information pages and stats.
 export async function getCrimesByCategoryAndCommunity(community) {
     try {
         const crimeSummary = CrimeRecord.aggregate([
@@ -42,6 +50,7 @@ export async function getCrimesByCategoryAndCommunity(community) {
                 }
             },
             {
+                // Again sort in decending order.
                 $sort: { totalCrimes: -1 }
             }
         ]);
