864ef8c66537ce18f99aeb8d382ae382fc51b212
ScottDavis08 <Scott.Davis08@gmail.com>
2025-04-19 13:20:53 -0600

    feat: incomplete changes to apply database sum levels to the colour pattern. Don't want to mess with stashing right now.

--- client/src/components/CommunityBoundaries.jsx
+++ client/src/components/CommunityBoundaries.jsx
@@ -5,6 +5,7 @@ import chroma from "chroma-js";
 
 function CommunityBoundaries() {
     const [communityBoundary, setCommunityBoundary] = useState([]);
+    const [maxCrime, setMaxCrime] = useState(0);
     const [error, setError] = useState(null);
 
     // When the page loads, this will run once.
@@ -14,19 +15,32 @@ function CommunityBoundaries() {
             try {
                 // Add all community records to a record.
                 const communities = await findAllCommunityBoundaries();
-                // Swap coordinates for Leaflet.
-                const communitiesWithSwappedCoords = communities.map(community => ({
-                    ...community,
-                    boundary: {
-                        ...community.boundary,
-                        // The nested .map functions allow the navigation down to the smallest element of the array.
-                        coordinates: community.boundary.coordinates.map(polygon =>
-                            polygon.map(ring =>
-                                ring.map(coord => [coord[1], coord[0]]) // Swaps lng,lat to lat,lng
-                            )
-                        )
+                const crimeSummary = await fetch('/api/crimeSummary').then(res => res.json());
+                console.log(`Crime Summary: ${JSON.stringify(crimeSummary)}`);
+
+                // Swap coordinates for Leaflet and add crime summaries.
+                const communitiesWithSwappedCoords = communities.map(community => {
+                    const crimeData = crimeSummary.find(crime => crime.commCode === community.commCode);
+
+                    // TODO: Investigate how to better do this once as a functionc all instead of testing every record.
+                    if (crimeData.totalCrimes > maxCrime) {
+                        setMaxCrime(crimeData.totalCrimes);
                     }
-                }))
+
+                    return {
+                        ...community,
+                        totalCrimes: crimeData ? crimeData.totalCrimes : 0,
+                        boundary: {
+                            ...community.boundary,
+                            // The nested .map functions allow the navigation down to the smallest element of the array.
+                            coordinates: community.boundary.coordinates.map(polygon =>
+                                polygon.map(ring =>
+                                    ring.map(coord => [coord[1], coord[0]]) // Swaps lng,lat to lat,lng
+                                )
+                            )
+                        }
+                    }
+                });
 
                 // Add the array of community boundaries to state.
                 setCommunityBoundary(communitiesWithSwappedCoords);
@@ -40,21 +54,14 @@ function CommunityBoundaries() {
     }, [])
 
     if (error) return <div>Error loading communities: {error}</div>
-    const minCrime = 0;
-    const maxCrime = 10000;
-
-    function getRandomColour(minCrime, maxCrime) {
-        return Math.floor(Math.random() * (maxCrime - minCrime) + minCrime);
-    }
 
     const scale = chroma.scale(['#00ff00', '#ffff00', '#ff0000'])
-        .domain([minCrime, maxCrime]);
+        .domain([0, maxCrime]);
 
     return communityBoundary.map((community) => {
-        const crimeValue =  getRandomColour(minCrime, maxCrime);
         const polygonOptions = {
-            color: scale(crimeValue ).hex(),
-            fillColor: scale(crimeValue ).hex(),
+            color: scale(community.totalCrimes).hex(),
+            fillColor: scale(community.totalCrimes).hex(),
             fillOpacity: 0.4,
             weight: 2
         }    // Draw the Polygon per each community that is returned.
