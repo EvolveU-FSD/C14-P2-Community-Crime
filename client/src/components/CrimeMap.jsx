0f8a2aa599a770ec5da5d2796a102dc77cd19119
ScottDavis08 <Scott.Davis08@gmail.com>
2025-04-26 21:48:51 -0600

    feat: Moved the map zoom control to the bottom left corner.

--- client/src/components/CrimeMap.jsx
+++ client/src/components/CrimeMap.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { FeatureGroup, MapContainer, TileLayer } from 'react-leaflet';
+import { FeatureGroup, MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import CommunityBoundaries from "./CommunityBoundaries";
 import BoundsUpdater from './BoundsUpdater';
@@ -11,7 +11,9 @@ export default function CrimeMap () {
         center={[51.0447, -114.0719]} // Calgary center
         zoom={12}
         className="leaflet-container"
+        zoomControl={false} // Disable default zoom control
       >
+        <ZoomControl position="bottomleft" /> {/* Add custom positioned zoom control */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
